bd88098e2c2e9eb444e0ad8f0e5b7630423c52e5
OHMAE Ryosuke <ryo@mm2d.net>
2020-10-08 08:39:46 +0900

    🆙firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ buildscript {
         classpath(kotlin("gradle-plugin", version = "1.4.10"))
         classpath("com.github.ben-manes:gradle-versions-plugin:0.29.0")
 
-        classpath("com.google.gms:google-services:4.3.3")
-        classpath("com.google.firebase:perf-plugin:1.3.1")
+        classpath("com.google.gms:google-services:4.3.4")
+        classpath("com.google.firebase:perf-plugin:1.3.2")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.3.0")
     }
 }
